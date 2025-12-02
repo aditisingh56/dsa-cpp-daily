aeec512080606791bba6cf25c5e761a00ac64d1e
Aditi Singh <aditisingh14006@gmail.com>
2025-12-03 00:15:43 +0530

    Brushing up basics

---
+++ Basics/Variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Variable declaration and initialization
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    // Output the values of the variables
+    cout << "Integer a: \n Float b:\n Character c:" << endl;
+  
+}

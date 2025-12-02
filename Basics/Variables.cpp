6ea020372b6c839b86adc0fbc395c6f08f180e2c
Aditi Singh <aditisingh14006@gmail.com>
2025-12-03 01:34:11 +0530

    Brushing very basics

--- Basics/Variables.cpp
+++ Basics/Variables.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main(){
     // Variable declaration and initialization
     int a = 10;
-    float b = 5.5;
+    float b = 5.5f;
     char c = 'A';
+    bool isSafe = true;
+    double d = 20.99;
     // Output the values of the variables
-    cout << "Integer a: \n Float b:\n Character c:" << endl;
+    cout << "Integer a: " << a << "\n Float b: " << b << "\n Character c: " << c << "\n bool value isSafe: " << isSafe << "\n double value are " << d << endl;
   
 }
---
+++ Basics/userOutputAndInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Variable declaration and initialization
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+
+    // Output the values of the variables
+    cout << "Integer a: " << a << endl;
+    cout << "Float b: " << b << endl;
+    cout << "Character c: " << c << endl;
+    int age ;
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    return 0;
+}

703e27e89f443199a3c79535a064d163225c819e
Aditi Singh <aditisingh14006@gmail.com>
2025-12-20 17:56:43 +0530

    Exploring STL - deque

---
+++ Level-00-Foundations/Cpp-Basics/STL/Deque.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<deque>
+#include<list>
+using namespace std;
+int main(){
+deque<int>d; // deque is very similar to list, but it allows random acces like vectors as internally it is implemented using arrays of fixed size
+d.push_back(10);
+d.push_back(20);
+d.push_front(5);
+cout<<"Elements in the deque are: ";
+for( auto it = d.begin(); it != d.end(); it++){
+    cout<<*it<<" ";
+
+}
+cout<<endl;
+d.pop_back();
+cout<<"Elements in the deque after popping back are: ";
+for( auto it = d.begin(); it != d.end(); it++){
+    cout<<*it<<" ";
+
+}
+return 0;
+}
